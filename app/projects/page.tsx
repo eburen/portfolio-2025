e805a40d3363f085aacd8565f443dbeaca2be18d
eburen <evrenxx34@gmail.com>
2025-05-13 20:56:49 +0900

    fixed a error on translation

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -208,8 +208,6 @@ function ProjectDetail({ project }: { project: Project }) {
 }
 
 export default function ProjectsPage() {
-    const { t } = useTranslation();
-
     return (
         <div className="py-20">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
